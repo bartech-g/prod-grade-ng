6e3406d51bed26bcc09c99a9d50638a7c0765d4b
bartech-g <bartech.js@gmail.com>
2024-04-28 16:23:16 +0200

    add libs, components, organize app.module

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,12 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreDataModule } from '@fem-production-angular/core-data';
+import { CoreStateModule } from '@fem-production-angular/core-state';
+import { MaterialModule } from '@fem-production-angular/material';
+import { StoreModule } from '@ngrx/store';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.module';
+import { WidgetDetailsComponent } from './widgets/widget-details/widget-details.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { WidgetsComponent } from './widgets/widgets.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    WidgetDetailsComponent,
+    WidgetsComponent,
+    WidgetsListComponent,
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    CoreDataModule,
+    CoreStateModule,
+    HttpClientModule,
+    MaterialModule,
+    RoutingModule,
+    StoreModule.forRoot({}, {}),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
